7726f3bc13ce00b8cc7e78576a94520373a4c1a2
Shyamjith06 <shyamjith.s06@gmail.com>
2022-10-06 09:56:19 +0530

    tes

---
+++ test/src/Selenium.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\shyam\\Downloads\\chromedriver_win32\\chromedriver.exe");	
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
--- test/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author shyam
- *
- */
-module test {
-}
